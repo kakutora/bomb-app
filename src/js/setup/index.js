5fceb5225ac3048c7a7da32352c4d5d4a5acdc4b
kakutora <ssbu.kak@gmail.com>
2023-06-29 11:32:29 +0900

    setupページ製作途中。飽きつつある。

--- src/js/setup/index.js
+++ src/js/setup/index.js
@@ -1,7 +1,15 @@
-import Swiper from "swiper";
-import 'swiper/css';
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
 import "../../css/setup/index.css";
 import io from 'socket.io-client';
-const swiper = new Swiper(".swiper", {
 
+const swiper = new Swiper(".swiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
